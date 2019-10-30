2e3e81f6d1eafa3948a14697e2689a44366b98a9
Praveen S <praveen26mysore@gmail.com>
2019-10-30 21:25:39 +0530

    Added images for FoodActivity

--- app/src/main/java/com/example/hungercure/Food.java
+++ app/src/main/java/com/example/hungercure/Food.java
@@ -4,17 +4,19 @@ public class Food {
 
     private String name;
     private String description;
+    private int imageId;
 
     public static final Food[] foods = {
 
-            new Food("Pizza", "Thin crust pizza with extra cheese"),
-            new Food("Burger", "Veg burger with healthy stuff"),
-            new Food("Sandwich", "Whole wheat sandwich")
+            new Food("Pizza", "Thin crust pizza with extra cheese", R.drawable.pizza),
+            new Food("Burger", "Veg burger with healthy stuff", R.drawable.burger),
+            new Food("Sandwich", "Whole wheat sandwich", R.drawable.sandwich)
     };
 
-    public Food(String name, String description) {
+    public Food(String name, String description, int imageId) {
         this.name = name;
         this.description = description;
+        this.imageId = imageId;
     }
 
     public String getName(){
@@ -27,5 +29,9 @@ public class Food {
 
     public String toString(){
         return this.name;
+    }
+
+    public int getImageId() {
+        return imageId;
     }
 }
--- app/src/main/java/com/example/hungercure/FoodActivity.java
+++ app/src/main/java/com/example/hungercure/FoodActivity.java
@@ -3,6 +3,7 @@ package com.example.hungercure;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class FoodActivity extends AppCompatActivity {
@@ -19,8 +20,10 @@ public class FoodActivity extends AppCompatActivity {
 
         TextView name = findViewById(R.id.name);
         TextView desc = findViewById(R.id.desc);
+        ImageView foodimage = findViewById(R.id.foodimage);
 
         name.setText(food.getName());
         desc.setText(food.getDescription());
+        foodimage.setImageResource(food.getImageId());
     }
 }
