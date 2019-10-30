62d612dfccfc15e69f1203bba1f4d54183006699
Praveen S <praveen26mysore@gmail.com>
2019-10-30 20:13:48 +0530

    Created Food class

---
+++ app/src/main/java/com/example/hungercure/Food.java
@@ -0,0 +1,31 @@
+package com.example.hungercure;
+
+public class Food {
+
+    private String name;
+    private String description;
+
+    public static final Food[] foods = {
+
+            new Food("Pizza", "Thin crust pizza with extra cheese"),
+            new Food("Burger", "Veg burger with healthy stuff"),
+            new Food("Sandwich", "Whole wheat sandwich")
+    };
+
+    public Food(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public String toString(){
+        return this.name;
+    }
+}
